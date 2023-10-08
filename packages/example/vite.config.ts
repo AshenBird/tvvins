c61b9b867cd40c3b7045c6e2afbdade763763933
McSwift <develop@mcswift.cn>
2023-10-08 15:45:46 +0800

    up

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -1,15 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import {resolve} from "node:path"
+import {cwd} from "node:process"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
   ],
+  publicDir:resolve(cwd(), "./public"),
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
