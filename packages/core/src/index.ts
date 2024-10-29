ff0c0a295fcaefcbf8a011bfb795431130c6bd6c
mcswift <develop@mcswift.cn>
2024-10-29 12:19:32 +0800

    up

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,11 +4,16 @@ import { App } from "./App";
 import { resolveOptions } from "./options";
 import { Tvvins } from "./type";
 import { logger } from "./logger";
+import process from "node:process";
 export * from "./type";
 export type { Context } from "./Context";
 export * from "./Middleware";
 
 export const useTvvins = (options: Tvvins.InitOptions) => {
+  // 全局错误处理
+  process.on("uncaughtException",(err,origin)=>{
+    logger.error(err)
+  })
   try{
     const mode = env["TVVINS_MODE"] as Tvvins.Mode
     const stage = env["TVVINS_STAGE"] as Tvvins.Stage
